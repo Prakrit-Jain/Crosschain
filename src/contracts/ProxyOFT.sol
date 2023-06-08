44930b10bcf7589fe3b0b3ad8b9559fb00f7c17b
Prakrit <pkjain@qodeleaf.com>
2023-06-08 17:00:17 +0530

    feat: Add contract for ProxyOFT

--- src/contracts/ProxyOFT.sol
+++ src/contracts/ProxyOFT.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.0;
 
-import "../OFTCore.sol";
+import "layerzero/token/oft/OFTCore.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract ProxyOFT is OFTCore {
