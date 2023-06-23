4a397a5a3226fb63e93536e8f32b22b0e7fecf97
Prakrit <pkjain@qodeleaf.com>
2023-06-23 12:49:31 +0530

    feat: Add Counter contract interface

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,14 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity =0.8.18;
 
-contract Counter {
-    uint256 public number;
-
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
-    }
-
-    function increment() public {
-        number++;
-    }
+/**
+ * @notice Counter Contract Interface To Call the Increment Function
+ */
+interface Counter {
+    /**
+     * @notice This function increments the value of storage variable by value `value`
+     * @param value The `value` to increase the variable by.
+     */
+    function increment(uint256 value) external;
 }
